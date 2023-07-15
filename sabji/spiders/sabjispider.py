a7e018e66a5f06d16912e302ad52f6c57237121e
Kiran Pantha <info@kiranpantha.com.np>
2023-07-15 23:00:16 +0545

    Update Scrapper

--- sabji/spiders/sabjispider.py
+++ sabji/spiders/sabjispider.py
@@ -1,15 +1,29 @@
 import scrapy
+import pandas as pd
+from datetime import datetime
+
 
 
 class SabjispiderSpider(scrapy.Spider):
     name = "sabjispider"
     allowed_domains = ["kalimatimarket.gov.np"]
 
+    date_list = pd.date_range(end = datetime.today(), periods = 100).to_pydatetime().tolist()
+
     start_urls = ['https://kalimatimarket.gov.np/price#']
 
     data_sabji = []
 
     def parse(self, response):
+        token = response.css('#csrf::attr(value)').get()
+        print({'datePricing': self.date_list[0].strftime("%Y-%m-%d"),'_token':token})
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'datePricing': self.date_list[0].strftime("%Y-%m-%d"),'_token':token},
+            callback=self.post_parse,
+        )
+
+    def post_parse(self,response):
         kalimati_wrapper = response.css('#commodityPriceParticular');
         for kalimati_tbody in kalimati_wrapper.css('tbody'):
             for kalimati_tr in kalimati_tbody.css('tr'):
