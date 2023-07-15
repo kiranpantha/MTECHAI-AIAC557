5939324c4479143d0ee04b2d91c7a4e278ba7f30
Kiran Pantha <info@kiranpantha.com.np>
2023-07-15 23:01:37 +0545

    Update Datelist Index Arr

--- sabji/spiders/sabjispider.py
+++ sabji/spiders/sabjispider.py
@@ -9,6 +9,7 @@ class SabjispiderSpider(scrapy.Spider):
     allowed_domains = ["kalimatimarket.gov.np"]
 
     date_list = pd.date_range(end = datetime.today(), periods = 100).to_pydatetime().tolist()
+    date_index = 0
 
     start_urls = ['https://kalimatimarket.gov.np/price#']
 
@@ -16,7 +17,7 @@ class SabjispiderSpider(scrapy.Spider):
 
     def parse(self, response):
         token = response.css('#csrf::attr(value)').get()
-        print({'datePricing': self.date_list[0].strftime("%Y-%m-%d"),'_token':token})
+        print({'datePricing': self.date_list[self.date_index].strftime("%Y-%m-%d"),'_token':token})
         return scrapy.FormRequest.from_response(
             response,
             formdata={'datePricing': self.date_list[0].strftime("%Y-%m-%d"),'_token':token},
